fddf3ca23625b0ad69e06377df0dbb0ec223ad4a
ginojasmin <ginojasmingj@gmail.com>
2025-06-25 08:11:51 +0530

    backend post API

--- FrontendandBackend Connection/backend/handlers/crudOperation/app.mjs
+++ FrontendandBackend Connection/backend/handlers/crudOperation/app.mjs
@@ -5,7 +5,8 @@ import { executeQuery } from '../../db/server.mjs';
 app.get('/details', async (req, res) => {
     console.log("GET /details endpoint called");
     try {
-        const data = await executeQuery('SELECT * FROM REACT_CRUDOPERATION');
+        let query = `SELECT * FROM REACT_CRUDOPERATION`
+        const data = await executeQuery(query);
         console.log("Fetched data:", data);
         res.json(data);
     } catch (err) {
@@ -14,6 +15,24 @@ app.get('/details', async (req, res) => {
     }
 });
 
+
+app.post('/details', async (req, res) => {
+    console.log("POST /details endpoint called");
+    try {
+        const { name, email, phone } = req.body;
+        const created_at = new Date();
+
+        let query = `INSERT INTO REACT_CRUDOPERATION(name,email,phone,created_at) VALUES(?, ?, ?, ?)`;
+let Values = [name, email, phone, created_at];
+
+        const data = await executeQuery(query, Values);
+        console.log("Inserted data:", data);
+        res.json(data);
+    } catch (err) {
+        console.error('Error inserting data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
